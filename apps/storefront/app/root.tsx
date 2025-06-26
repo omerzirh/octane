5190bf0c1a195ccce4a38ae696c9a2482d25519b
Omer Zirh <omerzirh@gmail.com>
2025-06-26 09:49:00 +0200

    of course didnt work

--- apps/storefront/app/root.tsx
+++ apps/storefront/app/root.tsx
@@ -107,43 +107,17 @@ export function ErrorBoundary() {
   const error = useRouteError();
 
   console.error('error boundary error', error);
-  const headRef = useRef<HTMLHeadElement>(null);
-  const data = useRootLoaderData();
 
-  const { env = {}, siteDetails } = data || {};
   return (
-    <RootProviders>
-      <html lang="en" className="min-h-screen">
-        <head ref={headRef}>
-          <meta charSet="UTF-8" />
-          <Meta />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet" />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Ballet:opsz@16..72&display=swap" rel="stylesheet" />
-
-          <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap" rel="stylesheet" />
-
-          <link href="https://fonts.googleapis.com/css2?family=Aboreto&display=swap" rel="stylesheet" />
-          <Links />
-          {siteDetails?.settings?.description && <meta name="description" content={siteDetails.settings.description} />}
-        </head>
-        <body className="min-h-screen">
-          <Page>
-            <Outlet />
-          </Page>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.ENV = ${JSON.stringify(env)}`,
-            }}
-          />
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    </RootProviders>
+    <html>
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Scripts />
+      </body>
+    </html>
   );
 }
